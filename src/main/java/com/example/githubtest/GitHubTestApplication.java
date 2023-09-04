f9ac5d8691283ca1f3e8ed98a99fdb813f0df79e
damir-cz98 <144006432+damir-cz98@users.noreply.github.com>
2023-09-04 14:15:59 +0300

    add changes

--- src/main/java/com/example/githubtest/GitHubTestApplication.java
+++ src/main/java/com/example/githubtest/GitHubTestApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitHubTestApplication {
 
     public static void main(String[] args) {
+//test comment
         SpringApplication.run(GitHubTestApplication.class, args);
+
     }
 
 }
